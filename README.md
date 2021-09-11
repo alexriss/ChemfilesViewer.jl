5bebc6197774c84484b75497d0fcf56205e2cd57
Alex Riss <00alexx@riss.at>
2021-09-11 21:49:31 +0200

    chemviewer_id is the reference (window not needed anymore)

--- README.md
+++ README.md
@@ -12,19 +12,28 @@ A julia library to visualize chemical molecules and other structures in the [Che
 ```julia
 # read molecule using Chemfiles
 using Chemfiles
-trajectory = Trajectory("mol.xyz")
+trajectory = Trajectory("mol.sdf")
 mol = read(trajectory)
 
 # render molecule
-w, _ = render_molecule(mol)
-w, _ = render_molecule(mol, w)  # render another molecule in the same window
+viewer_id = render_molecule(mol)
+viewer_id = render_molecule(mol, chemviewer_id=viewer_id)  # render another molecule in the same window
 
 # change properties
 d = generate_dict_molecule(mol)
 d["atoms"][1]["color"] = "#f00000"
 d["atoms"][1]["label"] = "Important atom"
-d["atoms"][1]["radius"] = 3
-w, _ = render_dict_molecule(d, w)
+d["atoms"][1]["radius"] = 1.0
+viewer_id = render_dict_molecule(d, chemviewer_id=viewer_id)
+
+# change view
+set_camera_position("x", "-")
+set_options(Dict("drawingType" => "wireframe"))
+set_camera_position("c", "+")
+set_options(Dict("drawingType" => "ball and stick"))
+
+# save image
+save_image("test.png")
 ```
 
 Use mouse to rotate, zoom and pan. Keyboard shortcuts `x`, `y`, `z` set the view along the x, y and z axis.
--- src/ChemfilesViewer.jl
+++ src/ChemfilesViewer.jl
@@ -11,8 +11,8 @@ export generate_dict_molecule, render_dict_molecule, render_molecule, set_camera
 
 DEBUG = false
 
-current_window = nothing
-current_chemviewer_id = ""
+current_chemviewer_id = ""  # holds the last used chemviewer_id
+chemviewer_id_window = Dict{String, Blink.Window}()  # holds the windows associated with the respective chemviewer_ids
 
 path_lib = normpath(@__DIR__, "..", "frontend")
 tpl_html = joinpath(path_lib, "app", "index.html")
@@ -76,33 +76,36 @@ end
 
 
 """
-    render_dict_molecule(dict_molecule::AbstractDict{String,<:Any}; window::Union{Blink.Window,Nothing}=nothing, options::AbstractDict{String,<:Any}=Dict{String,Any}())
+    function render_dict_molecule(dict_molecule::AbstractDict{String,<:Any}; chemviewer_id::String="", options::AbstractDict{String,<:Any}=Dict{String,Any}())
 
-Render the molecule from a dictionary containing the atoms, bonds and unit cell. If `window` is given, this window will be reused.
+Render the molecule from a dictionary containing the atoms, bonds and unit cell. If `chemviewer_id` is not given, a new electron window will be created.
 Additional `options` for rendering can be provided.
-Returns the current `window` instance and the id of the chemviewer javascript object.
+Returns the id of the current chemviewer javascript object.
 """
-function render_dict_molecule(dict_molecule::AbstractDict{String,<:Any}; window::Union{Blink.Window,Nothing}=nothing, options::AbstractDict{String,<:Any}=Dict{String,Any}())
-    if window === nothing
+function render_dict_molecule(dict_molecule::AbstractDict{String,<:Any}; chemviewer_id::String="", options::AbstractDict{String,<:Any}=Dict{String,Any}())
+    chemviewer_setup_needed = false
+    if chemviewer_id == ""
+        chemviewer_setup_needed = true
         window = Window(async=false)
+
         if DEBUG
             opentools(window)
         end
+        load!(window, include_js)
 
         html = read(tpl_html, String)
         chemviewer_id = string(UUIDs.uuid4())
 
-        # save current window and chemviewer_id
-        current_window = window
-        current_chemviewer_id = chemviewer_id
+        global chemviewer_id_window[chemviewer_id] = window
 
         html = replace(html, "{{ id }}" => chemviewer_id)
-
-        load!(window, include_js)
         body!(window, html)
+    else
+        window, chemviewer_id = get_window_chemviewer_id(chemviewer_id)
     end
 
-    json_molecule = JSON.json(dict_molecule)
+    # save current window and chemviewer_id
+    global current_chemviewer_id = chemviewer_id
 
     try
         # check if js functions are initialized
@@ -114,7 +117,10 @@ function render_dict_molecule(dict_molecule::AbstractDict{String,<:Any}; window:
             sleep(0.05)
         end
 
-        @js window setupChemViewer($options)
+        if chemviewer_setup_needed
+            @js window setupChemViewer($options)
+        end
+        json_molecule = JSON.json(dict_molecule)
         @js window drawJsonString($chemviewer_id, $json_molecule)
     catch e
         msg = sprint(showerror, e)
@@ -122,38 +128,38 @@ function render_dict_molecule(dict_molecule::AbstractDict{String,<:Any}; window:
         @show msg
         @show msg_full
     finally
-        return window, chemviewer_id
+        return chemviewer_id
     end
 end
 
 
 """
-    render_molecule(molecule::Chemfiles.Frame; window::Union{Blink.Window,Nothing}=nothing, options::AbstractDict{String,<:Any}=Dict{String,Any}())
+    function render_molecule(molecule::Chemfiles.Frame; chemviewer_id::String="", options::AbstractDict{String,<:Any}=Dict{String,Any}())
 
-Render the `molecule` (a Chemfiles frame). If `window` is not given, a new electron window will be created.
+Render the `molecule` (a Chemfiles frame). If `chemviewer_id` is not given, a new electron window will be created.
 Additional `options` for rendering can be provided.
-Returns the current `window` instance and the id of the chemviewer javascript object.
+Returns the id of the current chemviewer javascript object.
 """
-function render_molecule(molecule::Chemfiles.Frame; window::Union{Blink.Window,Nothing}=nothing, options::AbstractDict{String,<:Any}=Dict{String,Any}())
+function render_molecule(molecule::Chemfiles.Frame; chemviewer_id::String="", options::AbstractDict{String,<:Any}=Dict{String,Any}())
     dict_molecule = generate_dict_molecule(molecule)
-    return render_dict_molecule(dict_molecule, window=window, options=options)
+    return render_dict_molecule(dict_molecule, chemviewer_id=chemviewer_id, options=options)
 end
 
 
 """
-    set_camera_position(axis::String="z", direction::String="+"; window::Union{Blink.Window,Nothing}=nothing, chemviewer_id::String="")
+    function set_camera_position(axis::String="z", direction::String="+"; chemviewer_id::String="")
 
 Set the camera position to be along one of the axis `x`, `y`, `z` or the unit cell vectors `a`, `b`, `c`.
 The direction of `+` or `-` specifies in which direction the camera is moved.
-If the `window` and `chemviewer_id` are not specified, the most recent parameters are used. 
+If the `chemviewer_id` is not specified, the most recent instance is used. 
 """
-function set_camera_position(axis::String="z", direction::String="+"; window::Union{Blink.Window,Nothing}=nothing, chemviewer_id::String="")
+function set_camera_position(axis::String="z", direction::String="+"; chemviewer_id::String="")
     d = Dict(
         "cameraAxis" => axis,
         "cameraAxisDirection" => direction
     )
     
-    set_options(d, window=window, chemviewer_id=chemviewer_id)
+    set_options(d, chemviewer_id=chemviewer_id)
     return
 end
 
@@ -163,28 +169,24 @@ end
 
 Helper function to get the most recent `window` and `chemviewer_id` parameters. 
 """
-function get_window_chemviewer_id(window::Union{Blink.Window,Nothing}, chemviewer_id::String)
-    if window === nothing
-        window = current_window
-    end
-    if window === nothing
-        error("Can't get current window. Try specifying the window parameter manually.")
-        return nothing, nothing
-    end
+function get_window_chemviewer_id(chemviewer_id::String)
     if chemviewer_id == ""
         chemviewer_id = current_chemviewer_id
     end
     if chemviewer_id == ""
         error("Can't get current chemviewer_id. Try specifying the chemviewer_id parameter manually.")
-        return nothing, nothing
+        return nothing, ""
     end
 
-    return window, chemviewer_id
+    # save current window and chemviewer_id
+    global current_chemviewer_id = chemviewer_id
+    
+    return chemviewer_id_window[chemviewer_id], chemviewer_id
 end
 
 
 """
-    set_options(options::AbstractDict{String,<:Any}; window::Union{Blink.Window,Nothing}=nothing, chemviewer_id::String="")
+    function set_options(options::AbstractDict{String,<:Any}; chemviewer_id::String="")
 
 Set options for the render. Available options are: 
 
@@ -205,24 +207,24 @@ Set options for the render. Available options are:
 - `renderWidth`: width of the saved image, default is `1600`  
 - `renderHeight`: height of the saved image, default is `1600`
 
-If the `window` and `chemviewer_id` are not specified, the most recent parameters are used. 
+If the `chemviewer_id` is not specified, the most recent instance is used. 
 """
-function set_options(options::AbstractDict{String,<:Any}; window::Union{Blink.Window,Nothing}=nothing, chemviewer_id::String="")
-    window, chemviewer_id = get_window_chemviewer_id(window, chemviewer_id)
+function set_options(options::AbstractDict{String,<:Any}; chemviewer_id::String="")
+    window, chemviewer_id = get_window_chemviewer_id(chemviewer_id)
     @js window setOptions($chemviewer_id, $options)
     return
 end
 
 
 """
-    save_image(filename::AbstractString; window::Union{Blink.Window,Nothing}=nothing, chemviewer_id::String="")
+    function save_image(filename::AbstractString; chemviewer_id::String="")
 
 Save a png image of the render to `filename`. The image size is specified by the parameters `renderWidth` and `renderHeight`,
 which can be set by [`set_options`](@ref).
-If the `window` and `chemviewer_id` are not specified, the most recent parameters are used.
+If the `chemviewer_id` is not specified, the most recent instance is used. 
 """
-function save_image(filename::AbstractString; window::Union{Blink.Window,Nothing}=nothing, chemviewer_id::String="")
-    window, chemviewer_id = get_window_chemviewer_id(window, chemviewer_id)
+function save_image(filename::AbstractString; chemviewer_id::String="")
+    window, chemviewer_id = get_window_chemviewer_id(chemviewer_id)
 
     img_base64 = @js window get_png_string($chemviewer_id)
     img = base64decode(img_base64[23:end])
